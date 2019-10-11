dfdb749a321fdd0d4df50deb941c62b790cdc604
Schow94 <Schow94@gmail.com>
2019-10-10 19:55:56 -0700

    Updates

---
+++ precache-manifest.2e3c0812e82dda0908caa1a589e30b7c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e7e0071eff1e51e236ca2ab9094e486",
+    "url": "/Redux-TodoList/index.html"
+  },
+  {
+    "revision": "3984dcbc483e2cd2a597",
+    "url": "/Redux-TodoList/static/js/2.d129830b.chunk.js"
+  },
+  {
+    "revision": "19ac2723c3d36629b34a",
+    "url": "/Redux-TodoList/static/js/main.f394fd64.chunk.js"
+  },
+  {
+    "revision": "f8a30a8115cbc172fbf4",
+    "url": "/Redux-TodoList/static/js/runtime-main.e72f84c6.js"
+  }
+]);
